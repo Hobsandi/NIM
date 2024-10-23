0b62da5a131a621dd4317ee47717c63a5a92890d
Amadeus Stenqvist <amadeus.stenqvist.4732@student.uu.se>
2024-10-23 15:20:09 +0200

    lol test

--- obj/Debug/net8.0/nyttC#projekt.AssemblyInfo.cs
+++ obj/Debug/net8.0/nyttC#projekt.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("nyttC#projekt")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4125c397e4293a8e14bf63178559c68ead99ebc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("nyttC#projekt")]
 [assembly: System.Reflection.AssemblyTitleAttribute("nyttC#projekt")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
